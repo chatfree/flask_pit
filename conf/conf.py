330af88f65597287ca1c62eecc906ebcfadbbf80
huijie <huijie.deng@medishare.cn>
2017-11-30 20:01:05 +0800

    update

--- conf/conf.py
+++ conf/conf.py
@@ -3,7 +3,6 @@ import sys
 
 def getCfg(section,key):
   conf = configparser.ConfigParser()
-  print(sys.path[0])
   conf.read(sys.path[0]+"/conf/conf.cfg", encoding='utf-8')
   conf.sections()
   debug = conf.get(section,key)
